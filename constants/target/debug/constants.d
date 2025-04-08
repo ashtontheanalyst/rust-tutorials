f8be10af108cbef30ba195db7c6f235764d74f1c
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-04-08 12:19:17 -0500

    Added constants tutorial

---
+++ constants/target/debug/constants.d
@@ -0,0 +1,1 @@
+/home/dementus/code/rust/rust-tutorials/constants/target/debug/constants: /home/dementus/code/rust/rust-tutorials/constants/src/main.rs
---
+++ constants/target/debug/deps/constants-a0251e3431f798ac.d
@@ -0,0 +1,5 @@
+/home/dementus/code/rust/rust-tutorials/constants/target/debug/deps/constants-a0251e3431f798ac: src/main.rs
+
+/home/dementus/code/rust/rust-tutorials/constants/target/debug/deps/constants-a0251e3431f798ac.d: src/main.rs
+
+src/main.rs:
