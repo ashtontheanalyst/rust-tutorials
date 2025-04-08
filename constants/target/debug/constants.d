76387e7e670740f394c64aa7a53201c73584c3d2
Ashton Smith <138252806+ashtontheanalyst@users.noreply.github.com>
2025-04-08 12:19:57 -0500

    Delete constants directory

--- constants/target/debug/constants.d
+++
@@ -1,1 +0,0 @@
-/home/dementus/code/rust/rust-tutorials/constants/target/debug/constants: /home/dementus/code/rust/rust-tutorials/constants/src/main.rs
--- constants/target/debug/deps/constants-a0251e3431f798ac.d
+++
@@ -1,5 +0,0 @@
-/home/dementus/code/rust/rust-tutorials/constants/target/debug/deps/constants-a0251e3431f798ac: src/main.rs
-
-/home/dementus/code/rust/rust-tutorials/constants/target/debug/deps/constants-a0251e3431f798ac.d: src/main.rs
-
-src/main.rs:
